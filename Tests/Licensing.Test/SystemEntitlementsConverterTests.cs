7f0426933f7d535c38ce5ae204127c3194d88689
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-02-26 10:34:37 -0600

    Remove FluentAssertions

--- Tests/Licensing.Test/SystemEntitlementsConverterTests.cs
+++ Tests/Licensing.Test/SystemEntitlementsConverterTests.cs
@@ -3,7 +3,6 @@ namespace VerdantApp.Licensing.Test;
 using System.Globalization;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Time.Testing;
 using TIKSN.DependencyInjection;
